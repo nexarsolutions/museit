c61c70d6f698a8575bb6c28a28e1a424d28ec87e
Mansoor Satti <mansoorsatti201@gmail.com>
2025-12-16 17:34:26 +0500

    chore: Upgrade dependencies and Android SDK versions
    This commit updates several key dependencies to their latest versions and bumps the Android SDK configuration.
    
    ### Dependency Upgrades
    
    The following packages have been upgraded in `pubspec.yaml` to their latest stable versions to incorporate recent features and bug fixes:
    -   `pinput`: `^5.0.1` -> `^6.0.1`
    -   `audio_waveforms`: `^1.0.5` -> `^2.0.1`
    -   `google_sign_in`: `^6.2.0` -> `^7.2.0`
    
    The `pubspec.lock` file has been updated accordingly to reflect these and other transitive dependency changes.
    
    ### Android Configuration
    
    -   **`android/app/build.gradle.kts`**:
        -   The `compileSdk` has been updated from `35` to `36`.
        -   The `targetSdkVersion` has been updated from `35` to `36`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.nexar.musit"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -31,7 +31,7 @@ android {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.nexar.musit"
         minSdkVersion(24)
-        targetSdkVersion(35)
+        targetSdkVersion(36)
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
